29c919a34a016f92c3a144261fdb35728c048c91
IgorYev <123358377+IgorYev@users.noreply.github.com>
2024-05-29 17:31:04 +0300

    com 1

---
+++ src/basic/3.ts
@@ -0,0 +1,7 @@
+let firstExample: string | number;
+firstExample = "Hello";
+firstExample = 42;
+
+let secondExample: "enable" | "disable";
+secondExample = "enable";
+secondExample = "disable";
---
+++ src/basic/6.ts
@@ -0,0 +1,19 @@
+const mango = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
+
+// Створіть інтерфейс User для типізації об'єктів, які містять такі властивості. Зверніть увагу, що адреса є необов'язковою властивістю.
+
+const User = {};
